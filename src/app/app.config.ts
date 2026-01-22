24c64b240357478d41f04378f90c5bd0ec0e14db
Rohit Singh Aswal <rohitaswal1993@gmail.com>
2026-01-23 01:39:01 +0530

    changed aap config ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClient, provideHttpClient,withInterceptors } from '@angular/common/http';
+import { provideHttpClient,withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth-interceptor';
 export const appConfig: ApplicationConfig = {
